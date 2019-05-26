4abf2b30534186fd3d69f5e390df32cfbdbe86a9
codstice <48134435+codstice@users.noreply.github.com>
2019-05-26 15:05:31 +0900

    Update Target.cpp

--- Target.cpp
+++ Target.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<bitset>
 using namespace std;
+
 class Target_model{
-public:
+protected:
 	int GPIO_CDM; // 초기화는 파일입출력을 통해 받도록 한다. gpio_cdm의 구성요소는 아래 주석과 같다.
 	/*	bool clock_gating(전원연결 상태) //전원 켜짐(1) 꺼짐(0)
 		bool degital_enable; //중앙제어 프로그램에서 타겟에 대한 제어권한 있음(1) 없음(0)
@@ -13,4 +15,69 @@ public:
 	manCtl(int *rgst, int value){*rgst = value; }
 	virtual void autoCtl();
 	virtual void status();
+};
+
+
+class Printer : public Target_model{ // 초기화는 파일입출력을 통해 받도록 한다.
+public:
+	int ink; 
+	int paper;
+	int last;		
+
+    int *Register[4] = {&GPIO_CDM, &ink, &paper, &last};
+	virtual void status(){
+		cout<<"clock, degital_enable, mode : "<<bitset<3>(GPIO_CDM)<<endl;
+		cout<<"잉크 : "<<ink<<"%"<<endl;
+		cout<<"종이 : "<<paper<<"%"<<endl;
+		cout<<"마지막 변경시간 : "<<last<<endl<<endl;
+	}
+	virtual void autoCtl(int *rgst, int value){
+		cout<<"잉크(endl), 종이(0~100)"<<endl;
+		cout<<"프린터 유지보수 담당자에게 요구할 잉크의 양을 입력하세요 : ";
+		cin >>ink;
+		cout<<"프린터 유지보수 담당자에게 요구할 종이의 양을 입력하세요 : ";
+		cin >>paper;
+	}
+};
+
+class Aircon{ // 초기화는 파일입출력을 통해 받도록 한다.
+public:
+	int temper;
+	int operate;
+	int last;
+
+//	int *Register[4] = {&GPIO_CDM, &temper, &operate, &last};
+	virtual void status(){
+		cout<<"clock, degital_enable, mode : "<<bitset<3>(GPIO_CDM)<<endl;
+		cout<<"온도 : "<<temper<<"%"<<endl;
+		cout<<"운전방식(0:송풍 1:냉방 2:난방) : "<<operate<<"%"<<endl;
+		cout<<"마지막 변경시간 : "<<last<<"%"<<endl<<endl;
+	}
+	virtual void autoCtl(int *rgst, int value){
+		cout<<"온도(18~40), 운전방식(0:송풍 1:냉방 2:난방)"<<endl;
+		cout<<"온도를 입력하세요 : ";
+		cin >>temper;
+		cout<<"운전방식을 입력하세요 : ";
+		cin >>operate;
+	}
+};
+
+class Door{ // 초기화는 파일입출력을 통해 받도록 한다.
+public:
+	int is_open;
+    int is_error;
+	int last;
+
+//	int *Register[4] = {&GPIO_CDM, &is_open, &is_error, &last};
+	virtual void status(){
+		cout<<"clock, degital_enable, mode : "<<bitset<3>(GPIO_CDM)<<endl;
+		cout<<"개폐여부(0:닫힘 1:열림) : "<<is_open<<"%"<<endl;
+		cout<<"기능이상(0:이상무 1:이상) : "<<is_error<<"%"<<endl;
+		cout<<"마지막 변경시간 : "<<last<<endl<<endl;
+	}
+	virtual void autoCtl(int *rgst, int value){
+		cout<<"개폐방식(0:닫힘 1:열림)"<<endl;
+		cout<<"개폐방식을 입력하세요 : ";
+		cin >>is_open;
+	}
 };
