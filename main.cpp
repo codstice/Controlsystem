47bb42143289519a4b0d72687cc74bf191c5df29
hungry-shark <43433753+hungry-shark@users.noreply.github.com>
2019-05-27 01:41:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+//#include "timestamp.h"
 using namespace std;
 
 int main()
@@ -11,12 +12,15 @@ int main()
 	std::ifstream csv("D:/연습작품/visual studio data/cplus/test4.csv");
 	csv.ignore(500, '\n'); //첫 줄(세로 index)을 무시한다.
 	std::string line;
-	
-	vector <vector<float>> CDM_reg;
+
+	string log_GPIO_CDM[3];
+	vector <vector < float > > log_CDM_reg;
+	string log_CDM_time[3][2];
+
 	if (csv.is_open()) {
 		int row = 3; //배열의 인덱스를 세기위한 변수
-		int col = 4; 
-		CDM_reg.assign(row, vector<float>(col, 1));
+		int col = 5;
+		log_CDM_reg.assign(row, vector<float>(col, 1));
 
 		int i = 0; //i는 각종 장치들. 0번:printer 1번:aircon 2번:door. 
 		for (std::string row_line; std::getline(csv, row_line);)
@@ -30,18 +34,44 @@ int main()
 			int j = 0; //j는 각종 장치들의 레지스터들.
 			for (std::string column; std::getline(row_stream, column, ',');)
 			{
-				cout << stof(column.c_str()) << endl;
-				CDM_reg[i][j] = stof(column.c_str());
+				if (j == 0) //hex
+				{
+					log_GPIO_CDM[i] = column;
+					cout << log_GPIO_CDM[i] << endl; //확인용
+				}
+				else if (j == 3)
+				{
+					log_CDM_time[i][j-3] = column; //시간은 string data에 저장
+				}
+				else if (j == 4)
+				{
+					log_CDM_time[i][j-3] = column;
+					cout << "레지스터" << i << "의 최근 수정 시간 : " << log_CDM_time[i][j - 4] + " " + log_CDM_time[i][j-3] << endl; //확인용
+				}
+				else {
+					log_CDM_reg[i][j] = stof(column.c_str());
+					cout << stof(column.c_str()) << endl; //확인용
+				}
 				j++;
 			}
 			i++;
 		}
 
-		/************test용***************/
+		/***showing. if i=0 : printer , i=1 : aircon , i=2 : door***/
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				cout << CDM_reg[i][j] << endl;
+				if (j >= 3)
+				{
+					cout << log_CDM_time[i][j-3] << "\t";
+				}
+
+				else if (j == 0) {
+					cout << log_GPIO_CDM[i] << "\t";
+				}
+				else 
+					cout << log_CDM_reg[i][j] << "\t";
 			}
+			cout << endl;
 		}
 	}
 	else {
@@ -49,3 +79,31 @@ int main()
 	}
 }
 
+
+/*타임스탬프 구하는법
+https://kimgagakk.tistory.com/493 +
+int index = column.find_first_of("-");
+					string sub_str = column.substr(0, index);
+					int year = atoi(sub_str.c_str());
+					column = column.substr(index + 1);
+
+					index = column.find_first_of("-");
+					sub_str = column.substr(0, index);
+					int month = atoi(sub_str.c_str());
+
+					index = column.find_first_of(" ");
+					sub_str = column.substr(0, index);
+					int day = atoi(column.c_str());
+
+					index = column.find_first_of(":");
+					sub_str = column.substr(0, index);
+					int hour = atoi(column.c_str());
+
+					index = column.find_first_of(":");
+					sub_str = column.substr(0, index);
+					int minute = atoi(column.c_str());
+
+					column = column.substr(index+1);
+					int second = atoi(column.c_str());
+
+					//날짜 시간 나누는거 */
