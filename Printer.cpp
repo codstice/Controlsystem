6a792b29373f1a1798a55cd0994daf054f5d2239
codstice <48134435+codstice@users.noreply.github.com>
2019-05-26 14:30:32 +0900

    Update Printer.cpp

--- Printer.cpp
+++ Printer.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Printer {
-	/*상속대상*/
-	unsigned short *reg;
-	int data;
-	/*********/
-	int ink;
+class printer : public Target_model{ // 초기화는 파일입출력을 통해 받도록 한다.
+	/* 상속대상 */
+	int ink; 
 	int paper;
-	int last;
+	int last;		
 
-public:
-	void status() { 
-		cout << "잉크 잔여량 : " << ink << endl;
-		cout << "종이 잔여량 : " << paper << endl;
-		cout << "최근 작동 시간 : " << last << endl;
+    int *Register[4] = {&GPIO_CDM, &ink, &paper, &last};
+	virtual void status(){
+		cout<<"잉크 잔여량 : "<<ink<<"%\n"<<"종이 잔여량 : "<<paper<<"%\n"<<"마지막 변경시간 : "<<last<<"%\n"<<endl;
 	}
-	void autoCtl(unsigned short* R, int data) {
-		cout << "프린터가 자동작동됩니다" << endl;
-	}
-	void manCtl(unsigned short* R, int data) {
-		cout << "프린터가 수동작동됩니다" << endl;
+	virtual void autoCtl(int *rgst, int value){
+		cout<<"잉크(0~100) 종이(0~100)\n조작할 양을 입력하세요 : ";
+		cin >>ink>>paper;
 	}
 };
