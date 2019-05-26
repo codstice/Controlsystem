e7883b5eb2d30008686e42a8aa0e473d904d43a8
codstice <48134435+codstice@users.noreply.github.com>
2019-05-26 14:35:21 +0900

    Update Aircon.cpp

--- Aircon.cpp
+++ Aircon.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class Aircon  {
-	/*상속대상*/
-	unsigned short *reg;
-	int data;
-	int room;
-	/*********/
+class air_con{ // 초기화는 파일입출력을 통해 받도록 한다.
 	int temper;
 	int operate;
-	int last; //마지막 작동시간(로그기록)
-	int mode; //에어컨 작동모드
-	int room; //방의개수
+	int last;
 
-public:
-	void status() {
-		for (int i = 0; i < room; i++) {
-			if (mode == 1)
-				cout << i + 1 << "번방 에어컨 온도:" << temper << "도, " << "송풍" << endl;
-			else if (mode == 2)
-				cout << i + 1 << "번방 에어컨 온도:" << temper << "도, " << "난방" << endl;
-			else if (mode == 3)
-				cout << i + 1 << "번방 에어컨 온도:" << temper << "도, " << "냉방" << endl;
-			else //처음 mode 설정안하면 초기값 0
-				cout << i + 1 << "현재 에어컨이 꺼져있습니다." << endl;
-		}
-	}
-	void autoCtl(unsigned short* R, int data) {
-		cout << "에어컨이 자동작동됩니다" << endl;
-	}
-	void manCtl(unsigned short* R, int data) {
-		cout << "에어컨이 수동작동됩니다" << endl;
+//	int *Register[4] = {&GPIO_CDM, &temper, &operate, &last};
+	virtual void status(){cout<<"온도 : "<<temper<<"%\n"<<"운전방식(0:송풍 1:냉방 2:난방) : "<<operate<<"%\n"<<"마지막 변경시간 : "<<last<<"%\n\n";}
+	virtual void autoCtl(int *rgst, int value){
+		cout<<"온도(18~40) 운전방식(0:송풍 1:냉방 2:난방)\n조작할 양을 입력하세요 : ";
+		cin >>temper>>operate;
 	}
 };
+
+
