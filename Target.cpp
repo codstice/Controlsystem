78ee2ec8e8668caf8c00a0245472283c2c1152c9
codstice <48134435+codstice@users.noreply.github.com>
2019-06-03 16:58:33 +0900

    Update Target.cpp

--- Target.cpp
+++ Target.cpp
@@ -1,198 +1,376 @@
 #include <iostream>
-#include <bitset>
-#include <ctime>
+#include <stdlib.h>
+#include "Target.cpp"
+//File IO
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 #include <windows.h>
-#define font_blue() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY)
-#define font_red() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY)
-#define font_white() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY)
-#define font_green() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY)
-#define font_yellow() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY)
 
 using namespace std;
-int getMonth();
-int getHour();
-int getMinute();
 
-class Target_model {
-public:
-   int GPIO_CDM; // 초기화는 파일입출력을 통해 받도록 한다. gpio_cdm의 구성요소는 아래 주석과 같다.
-   //bool clock_gating(전원연결 상태) //전원 켜짐(1) 꺼짐(0)
-   //bool digital_enable; //중앙제어 프로그램에서 타겟에 대한 제어권한 있음(1) 없음(0)
-   //bool mode;   //타겟에 대한 자동제어(1) 수동제어(0)
+void read_csv_log(Printer *ptr0, Aircon *ptr1, Door *ptr2);
+void upLinePrompt(int count);
+void showConsole(Printer *printer1, Aircon *aircon1, Door *door1);
+void menu(Printer *printer1, Aircon *aircon1, Door *door1);
+bool fexists(const std::string& filename);
+
+int main() {
+   system("mode con cols=80 lines=55");
+   font_white();
+   Printer printer1;
+   Aircon aircon1;
+   Door door1;
+
+   if (!fexists("log.csv")) {
+      cout << "csv 파일을 찾을 수 없습니다. csv 파일을 생성합니다." << endl;
+      ofstream outFile("log.csv");
+      outFile << ",reg1,reg2,reg3,last,\nprinter,7,90,100,1559323990 ,\naircon,6,23,2,1559323990 ,\ndoor,7,1,0,1559323990 ," << endl;
+      outFile.close();
+      cout << "csv 파일을 정상적으로 생성하였습니다." << endl;
+   }
+
+   cout << "Start Central Control System Program..." << endl;
+   _sleep(1000);
+   read_csv_log(&printer1, &aircon1, &door1);
+
+   while (true) {
+   showConsole(&printer1, &aircon1, &door1);
+   cout << endl;
+   cout << "========================================================" << endl << endl;
+   cout << "printer status\n"; 
+   printer1.status() ;
+   cout << "aircon status\n";
+   aircon1.status();
+   cout << "door status\n";
+   door1.status() ;
+   cout << "========================================================" << endl << endl;
+   cout << "[*] CLI" << endl;
+   cout << "[>] ";
+
+      menu(&printer1, &aircon1, &door1);
+      _sleep(4000);
+      system("cls");
+   }
    
-   int value; //기기들의 내부 요소에 들어갈 수치다.
-   int manCtl(int *rgst, int v) { *rgst = v; GPIO_CDM |= 1; }
-   virtual void autoCtl() = 0;
-   virtual int status() = 0;
-};
-
-
-class Printer : public Target_model { // 초기화는 파일입출력을 통해 받도록 한다.
-public:
-   // ink, paper, last와 같은 요소들은 실제 임베디드 장치의 요소들이다. 보고서엔 "Register 배열이 있으므로 상용프로그램에서는 아래 변수는 필요없다"고 작성
-   int ink;
-   int paper;
-   int last;
-
-   int *Register[4] = { &GPIO_CDM, &ink, &paper, &last };
-   virtual int status() {
-      if (!(*Register[0] & 4)) {
-         cout <<"프린터의 전원이 꺼져있어 상태를 출력할 수 없습니다.\n\n";
-         return 0;
-      }
-      cout << "clock, digital_enable, mode : ";font_yellow();cout << "0b" << bitset<3>(*Register[0]) << endl;font_white();
-      cout << "잉크 : " << *Register[1] << "%" << endl;
-      cout << "종이 : " << *Register[2] << "%" << endl;
-      time_t last_to_time = *Register[3];
-      cout << "마지막 변경시간 : " << ctime(&last_to_time) << endl << endl;
-   }
-   virtual void autoCtl() {
-      if (!(*Register[0] & 4))
-      {
-         cout << "전원이 들어와있지 있습니다."; return;
-      }
-      if (!(*Register[0] & 2))
-      {
-         cout << "타겟 제어장치에서 타겟 회로로의 비트 입력권한이 없습니다."; return;
-      }
-      GPIO_CDM |= 1; //GPIO_CDM 에서 M비트(자동화 모드)를 강제로 1로 만든다.
-      cout << "잉크(0~100%), 종이(0~100%)" << endl;
-      cout << "프린터 유지보수 담당자에게 요구할 잉크의 양을 입력하세요 : ";
-      cin >> *Register[1];
-      cout << "프린터 유지보수 담당자에게 요구할 종이의 양을 입력하세요 : ";
-      cin >> *Register[2];
-      last = time(NULL); //수정시간 저장
-   }
-};
-
-class Aircon : public Target_model { // 초기화는 파일입출력을 통해 받도록 한다.
-public:
-   int temper;
-   int operate;
-   int last;
-
-   int *Register[4] = { &GPIO_CDM, &temper, &operate, &last };
-   virtual int status() {
-      if (!(*Register[0] & 4)) {
-         cout <<"에어컨의 전원이 꺼져있어 상태를 출력할 수 없습니다.\n\n";
-         return 0;
-      }
-      cout << "clock, digital_enable, mode : ";font_yellow();cout << "0b" << bitset<3>(*Register[0]) << endl;font_white();
-      cout << "온도 : " << *Register[1] << "＇" << endl;
-      cout << "운전방식(0:송풍 1:냉방 2:난방) : " << *Register[2] << endl;
-      time_t last_to_time = *Register[3];
-      cout << "마지막 변경시간 : " << ctime(&last_to_time) << endl << endl;
-   }
-   virtual void autoCtl() {
-      if (!(*Register[0] & 4))
-      {
-         cout << "전원이 들어와있지 있습니다."; return;
-      }
-      if (!(*Register[0] & 2))
-      {
-         cout << "타겟 제어장치에서 타겟 회로로의 비트 입력권한이 없습니다."; return;
-      }
-      GPIO_CDM |= 1; //GPIO_CDM 에서 M비트(자동화 모드)를 강제로 1로 만든다.
-      cout << "온도(18~40), 운전방식(0:송풍 1:냉방 2:난방)" << endl;
-      do {
-         cout << "온도를 입력하세요 : ";
-         cin >> *Register[1];
-      } while (*Register[1] < 18 || *Register[1] > 40);
-      cout << "운전방식을 입력하세요 : ";
-      cin >> *Register[2];
-      if (*Register[2] == 1 && (getMonth() == 11 || getMonth() == 12 || getMonth() == 1 || getMonth() == 2 || getMonth() == 3)) {
-         cout << "겨울에는 냉방기능을 사용할 수 없습니다." << endl;
-         *Register[2] = 2;
-         cout << "난방기능을 사용하도록 설정했습니다." << endl;
-      }
-      if (*Register[2] == 2 && (getMonth() == 11 || getMonth() == 6 || getMonth() == 7 || getMonth() == 8 || getMonth() == 9 || getMonth() == 10)) {
-         cout << "여름에는 난방기능을 사용할 수 없습니다." << endl;
-         *Register[2] = 1;
-         cout << "냉방기능을 사용하도록 설정했습니다." << endl;
-      }
-      last = time(NULL); //수정시간 저장
-   }
-};
-
-class Door : public Target_model { // 초기화는 파일입출력을 통해 받도록 한다.
-public:
-   int is_open;
-   int is_error;
-   int last;
-
-   int *Register[4] = { &GPIO_CDM, &is_open, &is_error, &last };
-   virtual int status() {
-      if (!(*Register[0] & 4)) {
-         cout <<"출입문의 전원이 꺼져있어 상태를 출력할 수 없습니다.\n\n";
-         return 0;
-      }
-      if (5 > getHour() && *Register[1] == 1) {
-         *Register[2] = 1;
-      }
-      else {
-         *Register[2] = 0;
-      }
-      if (!(5 > getHour()) && *Register[1] == 0) {
-         *Register[2] = 1;
-      }
-      else {
-         *Register[2] = 0;
-      }
-      GPIO_CDM |= 1; //GPIO_CDM 에서 M비트(자동화 모드)를 강제로 1로 만든다.
-      cout << "clock, digital_enable, mode : ";font_yellow();cout << "0b" << bitset<3>(*Register[0]) << endl;font_white();
-      cout << "개폐여부(0:닫힘 1:열림) : " << *Register[1] << endl;
-      cout << "기능이상(0:이상무 1:이상) : ";
-      if (*Register[2]) font_red();
-      cout << *Register[2] << endl;
-      font_white();
-      time_t last_to_time = *Register[3];
-      cout << "마지막 변경시간 : " << ctime(&last_to_time) << endl << endl;
-   }
-   virtual void autoCtl() {
-      if (!(*Register[0] & 4))
-      {
-         cout << "전원이 들어와있지 있습니다."; return;
-      }
-      if (!(*Register[0] & 2))
-      {
-         cout << "타겟 제어장치에서 타겟 회로로의 비트 입력권한이 없습니다."; return;
-      }
-      cout << "개폐방식(0:닫힘 1:열림)" << endl;
-      cout << "개폐방식을 입력하세요 : ";
-      cin >> *Register[1];
-      last = time(NULL); //수정시간 저장
-   }
-};
-
-int getMonth()
-{
-   time_t now;
-   struct tm *current_tm;
-
-   now = time(NULL);
-   current_tm = localtime(&now);
-
-   return current_tm->tm_mon + 1;
 }
 
-int getHour()
+// 콘솔 특정부분만 지워버림 (재출력할 필요가 없게됨) 사용하게 되면 필요함 https://blog.dork94.com/31
+void upLinePrompt(int count)
 {
-   time_t now;
-   struct tm *current_tm;
-
-   now = time(NULL);
-   current_tm = localtime(&now);
-
-   return current_tm->tm_hour;
+   for (int i = 0; i < count; ++i) {
+      //printf("%c[2K",27);
+      cout << "\33[2K"; //line clear
+      cout << "\x1b[A"; //up line (ESC [ A) must be support VT100 escape seq
+   }
 }
 
-int getMinute()
-{
-   time_t now;
-   struct tm *current_tm;
+void showConsole(Printer *_printer1_, Aircon *_aircon1_, Door *_door1_) {
 
-   now = time(NULL);
-   current_tm = localtime(&now);
+   //showconsole
+   cout << "                                ";font_red();cout<<"전원";font_blue();cout<<"   권한" ;font_green();cout<<"   자동모드  "<< endl;font_red();
+   cout << "┌───────────────────┐" << endl;
+   cout << "│                   │" << endl;
+   cout << "│    중 앙  제 어   │" << endl;
+   cout << "│                   │" << endl;
+   cout << "└───────────────────┘" << endl;
+   if (*((*_printer1_).Register[0]) & 4) font_red();else font_white();
+   cout << " │" << endl;
+   cout << " │" << endl;
+   cout << " └──────────";
+   if (*((*_printer1_).Register[0]) & 2) font_blue();else font_white();
+   cout << "───────── ";
+   font_white();
+   cout << "프린터    ";
+   font_red();
+   ((*((*_printer1_).Register[0]) & 4)) ? cout << "■     " : cout << "□     " ;font_blue();
+   ((*((*_printer1_).Register[0]) & 2)) ? cout << "■     " : cout << "□     " ;font_green();
+   ((*((*_printer1_).Register[0]) & 1)) ? cout << "■"<<endl : cout << "□"<<endl ;
+   if (*((*_aircon1_).Register[0]) & 4) font_red();else font_white();
+   cout << " │" << endl;
+   cout << " │" << endl;
+   cout << " └──────────";
+   if (*((*_aircon1_).Register[0]) & 2) font_blue();else font_white();
+   cout << "───────── ";
+   font_white();
+   cout << "에어컨    ";font_red();
+   ((*((*_aircon1_).Register[0]) & 4)) ? cout << "■     " : cout << "□     " ;font_blue();
+   ((*((*_aircon1_).Register[0]) & 2)) ? cout << "■     " : cout << "□     " ;font_green();
+   ((*((*_aircon1_).Register[0]) & 1)) ? cout << "■"<<endl : cout << "□"<<endl ;
 
-   return current_tm->tm_min;
+   if (*((*_door1_).Register[0]) & 4) font_red();else font_white();
+   cout << " │" << endl;
+   cout << " │" << endl;
+   cout << " └──────────";
+   if (*((*_door1_).Register[0]) & 2) font_blue();else font_white();
+   cout << "───────── ";
+   font_white();
+   cout << "출입문    ";
+   font_red();
+
+   ((*((*_door1_).Register[0]) & 4)) ? cout << "■     " : cout << "□     " ;font_blue();
+   ((*((*_door1_).Register[0]) & 2)) ? cout << "■     " : cout << "□     " ;font_green();
+   ((*((*_door1_).Register[0]) & 1)) ? cout << "■"<<endl : cout << "□"<<endl ;
+   font_white();
+
+  
+   //showConsole end 
+}
+void menu(Printer *printer1, Aircon *aircon1, Door *door1) {
+   int num = 7;
+   string Selectmenu;
+   int Selectnum;
+   cin >> Selectmenu;
+   cout << " " << endl;
+   if (!Selectmenu.compare("프린터")) {
+      cout << "1.전원 켜기/끄기\t\t2.잉크/종이 넣기 요청\t\t" << endl;
+      cout << "[>] ";
+      cin.ignore(); //cin의 버퍼를 비움
+      cin >> Selectnum;
+      cout << " " << endl;
+      if (Selectnum == 1) {
+         if (*(printer1->Register[0]) == 1)
+         {
+            cout << "프린터를 끄는중.";
+            for (int i = 0; i < 2; i++) {
+               for (int j = 0; j < 5; j++) {
+                  cout << ".";
+                  _sleep(1000);
+               }
+               cout << "\b\b\b\b\b";
+               cout << "     ";
+               cout << "\b\b\b\b\b";
+            }
+            cout << endl;
+            *(printer1->Register[0]) ^= 0x1 << 2; //3번째 비트(전원비트)만 반전
+            cout << "프린터가 꺼졌습니다." << endl;
+
+         }
+         else {
+            cout << "프린터를 키는중";
+            for (int i = 0; i < 2; i++) {
+               //cout << "\b\b\b\b\b";
+               for (int j = 0; j < 5; j++) {
+                  cout << ".";
+                  _sleep(1000);
+               }
+               cout << "\b\b\b\b\b";
+               cout << "     ";
+               cout << "\b\b\b\b\b";
+            }
+            cout << endl;
+            *(printer1->Register[0]) ^= 0x1 << 2; //3번째 비트(전원비트)만 반전
+            cout << "프린터가 켜졌습니다." << endl;
+         }
+      }
+      else if (Selectnum == 2) {
+         printer1->autoCtl();
+      }
+      //void upLinePrompt(int count)
+   }
+   else if (!Selectmenu.compare("에어컨")) {
+      cout << "1.전원 켜기/끄기\t\t2.희망온도 설정\t\t" << endl;
+      cout << "[>] ";
+      cin.ignore(); //cin의 버퍼를 비움
+      cin >> Selectnum;
+      cout << " " << endl;
+      if (Selectnum == 1) {
+         if (*(aircon1->Register[0]) & 4)
+         {
+            cout << "에어컨을 끄는중.";
+            for (int i = 0; i < 2; i++) {
+               for (int j = 0; j < 5; j++) {
+                  cout << ".";
+                  _sleep(1000);
+               }
+               cout << "\b\b\b\b\b";
+               cout << "     ";
+               cout << "\b\b\b\b\b";
+            }
+            cout << endl;
+            *(aircon1->Register[0]) ^= 0x1 << 2; //3번째 비트(전원비트)만 반전
+            cout << "에어컨이 꺼졌습니다." << endl;
+
+         }
+         else {
+            cout << "에어컨을 키는중";
+            for (int i = 0; i < 2; i++) {
+               //cout << "\b\b\b\b\b";
+               for (int j = 0; j < 5; j++) {
+                  cout << ".";
+                  _sleep(1000);
+               }
+               cout << "\b\b\b\b\b";
+               cout << "     ";
+               cout << "\b\b\b\b\b";
+            }
+            cout << endl;
+            *(aircon1->Register[0]) ^= 0x1 << 2; //3번째 비트(전원비트)만 반전
+            cout << "에어컨이 켜졌습니다." << endl;
+         }
+      }
+      else if (Selectnum == 2) {
+         aircon1->autoCtl();
+      }
+      //void upLinePrompt(int count)
+   }
+   else if (!Selectmenu.compare("출입문")) {
+      cout << "1.출입문 관리장치 전원 켜기/끄기\t\t2.개문/폐문 설정하기\t\t" << endl;
+      cout << "[>] ";
+      cin.ignore(); //cin의 버퍼를 비움
+      cin >> Selectnum;
+      cout << " " << endl;
+
+		if (Selectnum == 1) {
+		   if (*(door1->Register[0]) & 4)
+		   {
+		      cout << "출입문을 잠그는중.";
+		      for (int i = 0; i < 2; i++) {
+		         for (int j = 0; j < 5; j++) {
+		            cout << ".";
+		            _sleep(500);
+		         }
+		         cout << "\b\b\b\b\b";
+		         cout << "     ";
+		         cout << "\b\b\b\b\b";
+		      }
+		      cout << endl;
+		      *(door1->Register[0]) ^= 0x1 << 2; //3번째 비트(전원비트)만 반전
+		      cout << "출입문이 잠겼습니다." << endl;
+
+		   }
+		   else {
+		      cout << "출입문을 여는중";
+		      for (int i = 0; i < 2; i++) {
+		         //cout << "\b\b\b\b\b";
+		         for (int j = 0; j < 5; j++) {
+		            cout << ".";
+		            _sleep(500);
+		         }
+		         cout << "\b\b\b\b\b";
+		         cout << "     ";
+		         cout << "\b\b\b\b\b";
+		      }
+		      cout << endl;
+		      *(door1->Register[0]) ^= 0x1 << 2; //3번째 비트(전원비트)만 반전
+		      cout << "출입문이 열렸습니다." << endl;
+		   }
+		}else if (Selectnum == 2) {
+         door1->autoCtl();
+      }
+      //void upLinePrompt(int count)
+   }
+   else {
+      cout << " " << endl;
+      cout << "finding .." << endl;
+      _sleep(2000);
+      cout << "ERROR:연결된 장치가 없습니다" << endl;
+   }
+}
+
+
+// readlog file 
+
+
+int row = 3; //배열의 인덱스를 세기위한 변수
+int col = 5; //배열의 인덱스를 세기위한 변수
+string log_GPIO_CDM[3]; 
+vector <vector < unsigned int > > log_CDM_reg;
+string log_CDM_time[3][2];
+
+void read_csv_log(Printer *ptr0, Aircon *ptr1, Door *ptr2) { // 실행과 동시에 과거에 저장된 로그로부터 데이터를 수집하도록 한다.
+
+   std::ifstream csv("./log.csv");
+   csv.ignore(500, '\n'); //첫 줄(세로 index)을 무시한다.
+   std::string line;
+
+
+   if (csv.is_open()) {
+      
+      log_CDM_reg.assign(row, vector<unsigned int>(col, 1));
+
+      int i = 0; //i는 각종 장치들. 0번:printer 1번:aircon 2번:door. 
+      for (std::string row_line; std::getline(csv, row_line);) {
+         //첫 열(가로 index)을 무시한다.
+         const auto pos = row_line.find(',');
+         if (pos != string::npos)
+            row_line.erase(0, pos + 1);
+
+         std::istringstream row_stream(row_line);
+         int j = 0; //j는 각종 장치들의 레지스터들.
+         for (std::string column; std::getline(row_stream, column, ',');) {
+            if (j == 0) //hex
+            {
+               log_GPIO_CDM[i] = column;
+               //cout << log_GPIO_CDM[i] << endl; //확인용
+            }
+            else if (j == 3)
+            {
+               log_CDM_time[i][j - 3] = column; //시간은 string data에 저장
+            }
+            else if (j == 4)
+            {
+               log_CDM_time[i][j - 3] = column;
+               //cout << "레지스터" << i << "의 최근 수정 시간 : " << log_CDM_time[i][j - 4] + " " + log_CDM_time[i][j-3] << endl; //확인용
+            }
+            else {
+               log_CDM_reg[i][j] = stoi(column.c_str());
+               //cout << stof(column.c_str()) << endl; //확인용
+            }
+            j++;
+         }
+         i++;
+      }
+      cout << "loading log success\n" << endl;
+
+   }
+   else {
+      cout << "Unable to open file";
+   }
+   //nogada
+   //csv파일로 부터 읽은 정보를 클래스이 레지스터에 저장하는 코드
+   *(ptr0->Register[0]) = stoi(log_GPIO_CDM[0]);
+   *(ptr0->Register[1]) = log_CDM_reg[0][1];
+   *(ptr0->Register[2]) = log_CDM_reg[0][2];
+   *(ptr0->Register[3]) = stoi(log_CDM_time[0][0]);
+
+   *(ptr1->Register[0]) = stoi(log_GPIO_CDM[1]);
+   *(ptr1->Register[1]) = log_CDM_reg[1][1];
+   *(ptr1->Register[2]) = log_CDM_reg[1][2];
+   *(ptr1->Register[3]) = stoi(log_CDM_time[1][0]);
+
+   *(ptr2->Register[0]) = stoi(log_GPIO_CDM[2]);
+   *(ptr2->Register[1]) = log_CDM_reg[2][1];
+   *(ptr2->Register[2]) = log_CDM_reg[2][2];
+   *(ptr2->Register[3]) = stoi(log_CDM_time[2][0]);
+}
+void showdata() {
+   /***showing. if i=0 : printer , i=1 : aircon , i=2 : door***/
+   char target_name[3][10] = { "printer","aircon","door" };
+   cout << "\treg0\treg1 \treg2 \tlast" << endl;
+
+   for (int i = 0; i < row; i++) {
+      cout << target_name[i] << "\t";
+      for (int j = 0; j < col; j++) {
+         if (j >= 3)
+         {
+            cout << log_CDM_time[i][j - 3] << "\t";
+         }
+
+         else if (j == 0) {
+            cout << "0b" << bitset<3>(stoi(log_GPIO_CDM[i])) << "\t";
+         }
+         else
+            cout << log_CDM_reg[i][j] << "\t";
+      }
+      cout << endl;
+   }
+   cout << endl;
+}
+
+bool fexists(const std::string& filename) {
+  ifstream ifile(filename.c_str());
+  return (bool)ifile;
 }
