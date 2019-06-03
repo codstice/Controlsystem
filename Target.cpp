cd482b815c2be981c69740508f8c5135d72a2782
hungry-shark <43433753+hungry-shark@users.noreply.github.com>
2019-06-04 01:22:16 +0900

    Update Target.cpp
    last version

--- Target.cpp
+++ Target.cpp
@@ -12,14 +12,17 @@
 using namespace std;
 
 
-class Target_model {
+class Target_model { // initialized by File I/O 
 public:
-	int GPIO_CDM; // 초기화는 파일입출력을 통해 받도록 한다. gpio_cdm의 구성요소는 아래 주석과 같다.
-	//bool clock_gating(전원연결 상태) //전원 켜짐(1) 꺼짐(0)
-	//bool digital_enable; //중앙제어 프로그램에서 타겟에 대한 제어권한 있음(1) 없음(0)
-	//bool mode;   //타겟에 대한 자동제어(1) 수동제어(0)
+	int GPIO_CDM; 
+	//GPIO_CDM ? 
+	//Emebedded Calculate all values into 'Binary' 
+	//ex> 0b111 
+	//bool clock_gating // Connecting stats (on : 1 off : 0)
+	//bool digital_enable; //Centralized Control System's access permission to target device. (yes : 1 no : 0) 
+	//bool mode;   //Auto Control(1) Manual Control(0)
 
-	int value; //기기들의 내부 요소에 들어갈 수치다.
+	int value; // element values of devices 
 	int manCtl(int *rgst, int v) { *rgst = v; GPIO_CDM |= 1; }
 	virtual void autoCtl() = 0;
 	virtual int status() = 0;
